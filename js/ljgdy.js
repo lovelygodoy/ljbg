ecbf38ecfe15b93c5d55360420de3289c9fddbe4
lovelygodoy <lovelyjoygodoy@gmail.com>
2020-09-28 01:02:49 +0800

    new update

--- js/ljgdy.js
+++ js/ljgdy.js
@@ -1,7 +1,17 @@
-//Get the button
-var mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+var mybutton = document.getElementById("myBtn");
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 2000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("bg-load").style.display = "none";
+  document.getElementById("main").style.display = "block";
+}
+
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -12,8 +22,8 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
